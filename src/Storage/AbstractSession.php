27c4d88dd99cac61abe72f5c509141725b8010a4
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2025-01-20 11:11:01 -0500

    refactor(AbstractSession): improve type annotations

--- src/Storage/AbstractSession.php
+++ src/Storage/AbstractSession.php
@@ -5,36 +5,46 @@ declare(strict_types=1);
 namespace Phpolar\CsrfProtection\Storage;
 
 use ArrayAccess;
+use Phpolar\CsrfProtection\CsrfToken;
 
 /**
  * A testable session abstraction.
  *
- * @implements ArrayAccess<int|string,array<\Phpolar\CsrfProtection\CsrfToken>>
+ * @implements ArrayAccess<int|string,CsrfToken[]>
  */
 abstract class AbstractSession implements ArrayAccess
 {
     /**
-     * @param array<int|string,array<\Phpolar\CsrfProtection\CsrfToken>> $sessionVars
+     * @param array<int|string,CsrfToken[]> $sessionVars
      */
     public function __construct(private array &$sessionVars)
     {
     }
+
     public function offsetExists(mixed $offset): bool
     {
         return isset($this->sessionVars[$offset]) === true;
     }
-    public function offsetGet(mixed $offset): mixed
+
+    /**
+     * @param mixed $offset
+     * @return CsrfToken[]
+     */
+    public function offsetGet(mixed $offset): array
     {
         return $this->sessionVars[$offset];
     }
+
     public function offsetSet(mixed $offset, mixed $value): void
     {
         $this->sessionVars[$offset] = $value;
     }
+
     public function offsetUnset(mixed $offset): void
     {
         unset($this->sessionVars[$offset]);
     }
+
     /**
      * Determine if the session is not active.
      */
